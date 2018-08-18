5b728405fc8d888b2588d60983967f1b9fc7e60d
evpobr <evpobr@gmail.com>
2018-08-18 12:08:28 +0500

    Add library version retrieving functions, closes #2
    * artifact_get_version_major
    * artifact_get_version_minor
    * artifact_get_version_patch
    * artifact_get_version_tweak
    * artifact_get_version_string

--- include/artifact/artifact.h
+++ include/artifact/artifact.h
@@ -9,6 +9,12 @@ struct artifact_ctx;
 extern "C" {
 #endif
 
+ARTIFACT_EXPORT int artifact_get_version_major(void);
+ARTIFACT_EXPORT int artifact_get_version_minor(void);
+ARTIFACT_EXPORT int artifact_get_version_patch(void);
+ARTIFACT_EXPORT int artifact_get_version_tweak(void);
+ARTIFACT_EXPORT const char *artifact_get_version_string(void);
+
 int ARTIFACT_EXPORT artifact_new(struct artifact_ctx **ctx);
 unsigned long ARTIFACT_EXPORT artifact_unref(struct artifact_ctx *ctx);
 
--- src/artifact.c
+++ src/artifact.c
@@ -2,6 +2,22 @@
 
 #include <stdlib.h>
 
+const int VERSION_MAJOR = 0;
+const int VERSION_MINOR = 2;
+const int VERSION_PATCH = 0;
+const int VERSION_TWEAK = 0;
+const char *VERSION_STRING = "0.2.0";
+
+int artifact_get_version_major(void) { return VERSION_MAJOR; }
+
+int artifact_get_version_minor(void) { return VERSION_MINOR; }
+
+int artifact_get_version_patch(void) { return VERSION_PATCH; }
+
+int artifact_get_version_tweak(void) { return VERSION_TWEAK; }
+
+const char *artifact_get_version_string(void) { return VERSION_STRING; }
+
 int artifact_new(struct artifact_ctx **ctx)
 {
     struct artifact_ctx *c = calloc(1, sizeof(struct artifact_ctx));
